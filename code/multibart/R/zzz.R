31b60cd26946c7a8628d1981b8b3ed03531de9d6
Tristan Muno <tristan.muno@students.uni-mannheim.de>
2026-06-02 09:20:23 +0200

    multibart package by `yeager2022synergistic`

---
+++ code/multibart/R/zzz.R
@@ -0,0 +1,13 @@
+#' multibart
+#'
+#' Description of your package
+#'
+#' @docType package
+#' @import Rcpp
+#' @importFrom Rcpp evalCpp
+#' @useDynLib multibart
+#' @name multibart
+#' @export tree_samples 
+NULL
+
+loadModule("tree_samples", TRUE)
